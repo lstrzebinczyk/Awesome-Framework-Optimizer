d1080fd473fc54ae74eaf3297e4ee5567ecbafce
Killa <killa@ubuntu.(none)>
2013-04-15 18:51:58 +0200

    Inlined Framework modules to Framework class, moved Framework to helper_classes/

--- lib/helper_classes/framework.rb
+++ lib/helper_classes/framework.rb
@@ -1,4 +1,227 @@
-module FrameworkTriangulation
+class Framework
+  attr_accessor :points, :lines, :polygons, :stiff, :constants
+  attr_accessor :max_goal, :max_energy
+
+  def config
+    @configuration = Configuration.global
+  end
+
+  def draw_empty
+    Presenter.new(self).draw_empty
+  end
+
+  def draw_status_1
+    Presenter.new(self).draw_status_1
+  end
+
+  def draw_status_2
+    Presenter.new(self).draw_status_2
+  end
+
+  def select_new_points
+    dividing_polygon = polygons.max_by{|poly| poly.energy(self.stiff)}
+    @new_points += dividing_polygon.dividing_points
+    @points += @new_points
+
+    @points.each_index do |i|
+      @points[i].id = i
+    end
+
+    return dividing_polygon.energy(self.stiff) > self.constants.energy_limit
+  end
+
+  def preprocessor
+    @fem = FemEquation.new(self)
+  end
+
+  def processor
+    @deltas = @fem.solve
+  end
+
+  def postprocessor
+    points.each_with_index do |point, i|
+      point.change_delta(@deltas[2 * i], @deltas[2 * i + 1])
+    end
+  end
+
+  def optimize
+    poly = find_smelly_polygon
+    remove_polygon(poly)
+    self.reload
+    self.FEM_solve
+    return self.goal
+  end
+
+  def find_smelly_polygon
+    self.polygons.max_by{|polygon| polygon.deletion_goal(self.stiff)}
+  end
+
+  def remove_polygon(poly)
+    polygons.delete(poly)
+    delete_line(poly.p1, poly.p2) if delete_line?(poly.p1, poly.p2)
+    delete_line(poly.p3, poly.p2) if delete_line?(poly.p3, poly.p2)
+    delete_line(poly.p1, poly.p3) if delete_line?(poly.p1, poly.p3)
+    delete_point poly.p1 if delete_point?(poly.p1)
+    delete_point poly.p2 if delete_point?(poly.p2)
+    delete_point poly.p3 if delete_point?(poly.p3)
+  end
+
+  def has_point?(point)
+    self.polygons.any? {|poly| poly.has_point?(point)}
+  end
+
+  def has_line?(point1, point2)
+    self.polygons.any? {|poly| poly.has_line?(point1, point2)}
+  end
+
+  def delete_point?(point)
+    not has_point? point
+  end
+
+  def delete_line?(p1, p2)
+    not has_line?(p1, p2)
+  end
+
+  def delete_line(p1, p2)
+    lines.delete_if{|line| (line.p1 == p1 and line.p2 == p2) or (line.p1 == p2 and line.p2 == p1)}
+  end
+
+  def delete_point(p1)
+    points.delete p1
+  end
+
+  def initialize
+    @points = []
+
+    @points << Point.new(0, 0)
+    @points << Point.new(10, 0)
+    @points << Point.new(15, 0)
+    @points << Point.new(0, 30)
+    @points << Point.new(10, 30)
+    @points << Point.new(15, 30)
+    @points << Point.new(8, 14)
+    @points << Point.new(15, 14)
+    @points << Point.new(15, 10)
+    @points << Point.new(8, 10)
+
+    @lines = []
+    @polygons = []
+    @stiff = config.stiff
+    @constants = config
+    @new_points = []
+    @max_goal = 0
+    @max_energy = 0
+
+    self.reload
+    
+    self
+  end
+
+  def make_denser
+    self.perform_triangulation
+    self.reload
+    self.FEM_solve
+    self.count_field_and_energy
+    self.perform_multigrid
+  end
+
+  def perform_triangulation
+    if @new_points.empty?
+      #performs basic delaunay triangulation using Bowyer-Watson Algorithm
+      basic_triangulation
+    else
+      triangulate_new_points
+    end
+
+    #performs mesh refinement using ruppert algorith
+    ruppert_refinement
+  end
+
+  #This method actually counts Fem thing and actualizes dx and dy
+  def FEM_solve
+    #creates and remembers fem equation
+    preprocessor
+
+    #solves fem equation
+    processor
+
+    #inserts fem result into mesh displacement
+    postprocessor
+  end
+
+  def perform_multigrid
+    select_new_points
+  end
+
+  def perform_optimizing
+    optimize
+  end
+
+  #This method sets lines and polygons to clear arrays
+  def reset
+    self.lines = []
+    self.polygons = []
+  end
+
+  #resets data in points
+  def reload
+    #reset force put to points
+    points_to_input_force = []
+    points_to_block = []
+    points_to_x_block = []
+
+    points.each_index do |i|
+      points[i].reset
+      points[i].id = i
+      points_to_input_force << points[i] if points[i].y == 30 and points[i].x >= 10 and points[i].x <= 20
+      points_to_block << points[i] if points[i].y == 0 and points[i].x >= 10 and points[i].x <= 20
+      points_to_x_block << points[i] if points[i].x == 15
+    end
+
+    force = constants.force / points_to_input_force.length
+
+    points_to_input_force.each do |pt|
+      pt.fy = force
+    end
+
+    points_to_block.each do |pt|
+      pt.block
+    end
+
+    points_to_x_block.each do |pt|
+      # puts "#{pt.x} + #{pt.y}"
+      pt.set_block_x
+    end
+  end
+
+  def count_field_and_energy
+    @max_goal = 0
+    @max_energy = 0
+
+    polygons.each do |poly|
+      poly.deletion_goal = nil
+      poly.energy = nil
+      @max_goal = poly.deletion_goal(self.stiff) if poly.deletion_goal(self.stiff) > @max_goal
+      @max_energy = poly.energy(self.stiff) if poly.energy(self.stiff) > @max_energy
+    end
+  end
+            
+  def goal
+    self.field * self.displacement
+  end
+
+  def displacement
+    (self.points.min_by{|pt| pt.dy}).dy.abs
+  end
+
+  def field
+    sum = 0
+    polygons.each do |poly|
+      sum += poly.field
+    end
+    sum
+  end
+
 
   #Bowyer-Watson Algorithm
   def basic_triangulation
